f127cf84f1f32eeb4ca699b8326da8a247566175
Rafael Neves <rafael.neves@cognizant.com>
2020-01-23 21:04:41 -0300

    first commit

--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,10 @@ export function delay(ms) {
 }
 
 export const camelize = str => {
-  return removeDiacritics(str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()));
+  // return removeDiacritics(str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()));
+
+  const string = removeDiacritics(str);
+  return string.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
 };
 
 export const removeDiacritics = str => {
