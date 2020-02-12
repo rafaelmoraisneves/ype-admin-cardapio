8c81cceb6f863b8fa180cf832d5e8d451349149d
Rafael Neves <rafael.neves@cognizant.com>
2020-02-12 16:55:09 -0300

    fixing request bug

--- src/index.js
+++ src/index.js
@@ -531,7 +531,7 @@ async function editFormView(event) {
     $(`.input-container[data-type="${type}"]`).last().after(`
       <div class="input-container" data-type="${type}">
         <p class="input-help">Adicione a opção</p>
-        <input type="text" placeholder="Insira a opção" name="caseiro-saladas[]"/>
+        <input type="text" placeholder="Insira a opção" name="caseiro-salada[]"/>
         <div class="remove-input" data-type="${type}"><i class="ms-Icon ms-Icon--ChromeClose" aria-hidden="true"></i></div>
       </div>`);
 
@@ -749,7 +749,7 @@ async function detailsView(event) {
         arrPrato[4] = elm.Prato.find(prato => prato.tipoPrato === 'Frutas')
 
         let pratos = arrPrato.map(elm => {
-
+          if(!elm) return
 
           if(!elm.Prato) return
          
